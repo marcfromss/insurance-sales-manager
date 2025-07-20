4c4d1cf5c0fe214cc8721b44bfd61df02af207ff
digital World <digitalworld@digitals-MacBook-Air.local>
2025-07-20 15:59:25 -0400

    Remove app directory to avoid routing conflicts

--- app/layout.js
+++
@@ -1,9 +0,0 @@
-import '../styles/globals.css';
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className="bg-gray-100 text-gray-900 min-h-screen font-sans p-6">{children}</body>
-    </html>
-  );
-} 
