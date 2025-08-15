42782e2e350b6e56c1bc451bc89e6ae575a854f6
digital World <digitalworld@digitals-MacBook-Air.local>
2025-08-15 11:46:20 -0400

    Revert branding to Diamond Coverage red gem, move fonts to _document, remove App Router remnants

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,5 @@
 import '../styles/globals.css';
-import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Great+Vibes&family=Manrope:wght@600;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
-} 
+  return <Component {...pageProps} />;
+}
